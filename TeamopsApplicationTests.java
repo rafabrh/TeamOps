814303f9828d5724775a17dd9ac2b7cb72af9cc4
rafabrh <rafael.alvarenga1@hotmail.com>
2025-09-17 06:12:36 -0300

    CI verde: profile de teste com Testcontainers; corrige duplicatas no POM; adiciona ADR-0001..0003 e workflow

---
+++ TeamopsApplicationTests.java
@@ -0,0 +1,12 @@
+package com.rafadev.teamops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class TeamopsApplicationTests {
+    @Test
+    void contextLoads() { }
+}
